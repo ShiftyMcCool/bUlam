56d31a0a1d972e6a2111e88c357620149fda85bc
Ryan Harris <ryaneharris@gmail.com>
2014-06-15 20:13:59 -0700

    Added navbar file and necessary refactor

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -2,33 +2,28 @@
 
 var controllers = angular.module('controller', []);
 
-controllers.controller('alphabetController', function($scope,$route,$location,alphabet,vowels,consonants,puncMarks) {
+controllers.controller('navController', function($scope,$route,$location) {
+	$scope.navbar = {name: "navbar.html", url: "views/navbar.html"};
+	$scope.isActive = function(route) {
+		return route === $location.path();
+	}
+});
+
+controllers.controller('alphabetController', function($scope,vowels,consonants,puncMarks) {
 	$scope.vowels = vowels;
 	$scope.consonants = consonants;
 	$scope.puncMarks = puncMarks;
-	$scope.isActive = function(route) {
-		return route === $location.path();
-	}
 });
 
-controllers.controller('ruleController', function($scope,$route,$location,rule,rules) {
+controllers.controller('ruleController', function($scope,rules) {
 	$scope.rules = rules;
-	$scope.isActive = function(route) {
-		return route === $location.path();
-	}
 });
 
-controllers.controller('verbController', function($scope,$route,$location,verb,verbs) {
+controllers.controller('verbController', function($scope,verbs) {
 	$scope.verbs = verbs;
-	$scope.isActive = function(route) {
-		return route === $location.path();
-	}
 });
 
-controllers.controller('numberController', function($scope,$route,$location,number,numbers,equations) {
+controllers.controller('numberController', function($scope,numbers,equations) {
 	$scope.numbers = numbers;
 	$scope.equations = equations;
-	$scope.isActive = function(route) {
-		return route === $location.path();
-	}
 });
